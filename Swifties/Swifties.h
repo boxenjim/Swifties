2b4368b4dab5fa834bbeab263a593faf6870eb23
Jim Schultz <boxenjim@gmail.com>
2015-02-11 11:49:08 -0800

    Initial Commit

---
+++ Swifties/Swifties.h
@@ -0,0 +1,19 @@
+//
+//  Swifties.h
+//  Swifties
+//
+//  Created by Jim Schultz on 2/11/15.
+//  Copyright (c) 2015 Blue Boxen, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifties.
+FOUNDATION_EXPORT double SwiftiesVersionNumber;
+
+//! Project version string for Swifties.
+FOUNDATION_EXPORT const unsigned char SwiftiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifties/PublicHeader.h>
+
+
