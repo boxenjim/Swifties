d98784de6fb1c3008b887777edfac71a50ad3bc2
Jim Schultz <boxenjim@gmail.com>
2015-03-16 10:25:38 -0700

    cleaned up open url method, not checking urlschemes from bundle for now

--- Swifties/Switchboard.swift
+++ Swifties/Switchboard.swift
@@ -10,29 +10,15 @@ import UIKit
 
 public class Switchboard: NSObject {
     public func open(url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
-        if let urlTypes = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleURLTypes") as? NSArray {
-            for urlType in urlTypes {
-                if let type = urlType as? NSDictionary {
-                    if let schemes = type["CFBundleURLSchemes"] as? NSArray {
-                        for urlScheme in schemes {
-                            if let scheme = urlScheme as? NSString {
-                                // do something important here, grab url components etc.
-                                let comps = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
-                                println("COMPS: \(comps)")
-                                if let queryItems = comps?.queryItems {
-                                    for queryItem in queryItems {
-                                        if let query = queryItem as? NSURLQueryItem {
-                                            println("QUERY: key: \(query.name) value: \(query.value)")
-                                        }
-                                    }
-                                }
-                                return true
-                            }
-                        }
-                    }
+        let comps = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
+        println("COMPS: \(comps)")
+        if let queryItems = comps?.queryItems {
+            for queryItem in queryItems {
+                if let query = queryItem as? NSURLQueryItem {
+                    println("QUERY: key: \(query.name) value: \(query.value)")
                 }
             }
         }
-        return false
+        return true
     }
 }
